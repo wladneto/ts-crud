01c42bfa9b9d417fbfe005e7911cf43486b2f182
WladNeto <wladimirteixeira@yahoo.com.br>
2022-10-10 11:28:21 -0300

    remove emoji route and tests

--- api-express/src/api/index.ts
+++ api-express/src/api/index.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 
 import MessageResponse from '../interfaces/MessageResponse';
-import emojis from './emojis';
 import todos from './todos/todos.routes';
 
 const router = express.Router();
@@ -10,9 +9,8 @@ router.get<{}, MessageResponse>('/', (req, res) => {
   res.json({
     message: 'API - 👋🌎🌍🌏',
   });
-});
+})
 
-router.use('/emojis', emojis);
 router.use('/todos', todos);
 
 export default router;
