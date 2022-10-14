63411d5133edcfdcb553f013b371b7fd1f1c172b
WladNeto <wladimirteixeira@yahoo.com.br>
2022-10-14 20:41:40 +0800

    remove folder api-express

--- api-express/src/db.ts
+++
@@ -1,9 +0,0 @@
-import {MongoClient} from 'mongodb';
-
-
-const {
-    MONGO_URI = "mongodb+srv://user:password@server/database",
-} = process.env;
-
-export const client = new MongoClient(MONGO_URI);
-export const db = client.db();
